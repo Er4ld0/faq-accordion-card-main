0110aaadafed130c81b4a0ec987fca2cbef53372
Er4ld0 <junior-eraldo2011@hotmail.com>
2023-11-29 23:06:00 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,160 @@
+function clickFaq1(){
+    if (title1.style.display == 'block'){
+        title1.style.display = 'none'
+    } else{
+        title1.style.display = 'block'
+    }
+    
+    if (faq1.style.rotate == '-180deg'){
+        faq1.style.rotate = '0deg'
+    }   else{
+        faq1.style.rotate = '-180deg'
+    }
+
+    if (faq1.style.transition == '0.5s'){
+        faq1.style.transition = '0'
+    }   else{
+        faq1.style.transition = '0.5s'
+    }
+}
+
+/*===================================================================*/ 
+
+function clickFaq2(){
+    if (title2.style.display == 'block'){
+        title2.style.display = 'none'
+    } else{
+        title2.style.display = 'block'
+    }
+
+    if (faq2.style.rotate == '180deg'){
+        faq2.style.rotate = '0deg'
+    }   else{
+        faq2.style.rotate = '180deg'
+    }
+
+    if (faq2.style.transition == '0.5s'){
+        faq2.style.transition = '0'
+    }   else{
+        faq2.style.transition = '0.5s'
+    }
+    
+}
+
+/*===================================================================*/ 
+
+function clickFaq3(){
+    if (title3.style.display == 'block'){
+        title3.style.display = 'none'
+    } else{
+        title3.style.display = 'block'
+    }
+
+    if (faq3.style.rotate == '-180deg'){
+        faq3.style.rotate = '0deg'
+    }   else{
+        faq3.style.rotate = '-180deg'
+    }
+
+    if (faq3.style.transition == '0.5s'){
+        faq3.style.transition = '0'
+    }   else{
+        faq3.style.transition = '0.5s'
+    }
+}
+
+/*===================================================================*/ 
+
+function clickFaq4(){
+    if (title4.style.display == 'block'){
+        title4.style.display = 'none'
+    } else{
+        title4.style.display = 'block'
+    }
+
+    if (faq4.style.rotate == '180deg'){
+        faq4.style.rotate = '0deg'
+    }   else{
+        faq4.style.rotate = '180deg'
+    }
+
+    if (faq4.style.transition == '0.5s'){
+        faq4.style.transition = '0'
+    }   else{
+        faq4.style.transition = '0.5s'
+    }
+}
+
+/*===================================================================*/ 
+
+function clickFaq5(){
+    if (title5.style.display == 'block'){
+        title5.style.display = 'none'
+    } else{
+        title5.style.display = 'block'
+    }
+
+    if (faq5.style.rotate == '-180deg'){
+        faq5.style.rotate = '0deg'
+    }   else{
+        faq5.style.rotate = '-180deg'
+    }
+
+    if (faq5.style.transition == '0.5s'){
+        faq5.style.transition = '0'
+    }   else{
+        faq5.style.transition = '0.5s'
+    }
+}
+
+/*===================================================================*/
+/*===================================================================*/ 
+/*===================================================================*/  
+
+function activeFaq1(){
+    if (item_title.style.color == 'black'){
+        item_title.style.color = 'var(--very_dark_grayish_blue)'
+    }   else{
+        item_title.style.color = 'black'
+    }
+}
+
+/*===================================================================*/ 
+
+function activeFaq2(){
+    if (item_title2.style.color == 'black'){
+        item_title2.style.color = 'var(--very_dark_grayish_blue)'
+    }   else{
+        item_title2.style.color = 'black'
+    }
+}
+
+/*===================================================================*/ 
+
+function activeFaq3(){
+    if (item_title3.style.color == 'black'){
+        item_title3.style.color = 'var(--very_dark_grayish_blue)'
+    }   else{
+        item_title3.style.color = 'black'
+    }
+}
+
+/*===================================================================*/ 
+
+function activeFaq4(){
+    if (item_title4.style.color == 'black'){
+        item_title4.style.color = 'var(--very_dark_grayish_blue)'
+    }   else{
+        item_title4.style.color = 'black'
+    }
+}
+
+/*===================================================================*/ 
+
+function activeFaq5(){
+    if (item_title5.style.color == 'black'){
+        item_title5.style.color = 'var(--very_dark_grayish_blue)'
+    }   else{
+        item_title5.style.color = 'black'
+    }
+}
